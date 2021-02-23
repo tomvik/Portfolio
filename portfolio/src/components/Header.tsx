c08ad9b82ba4b725ab01a9c404d2a2b9b5f717a0
Tomas Lugo <lugo.lbmi@gmail.com>
2021-02-22 20:56:11 -0600

    Adds base Header
    - Changes eslint to 2 spaces
    - Adds react-router-dom
    - Makes three simple pages

---
+++ portfolio/src/components/Header.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Container,
+} from "@material-ui/core";
+import { Home } from "@material-ui/icons";
+import kNavLinks from "../urls";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  navbarDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`,
+  },
+  navDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`,
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `white`,
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Container maxWidth="md" className={classes.navbarDisplayFlex}>
+          <IconButton edge="start" color="inherit" aria-label="home">
+            <Home fontSize="large" />
+          </IconButton>
+          <List
+            component="nav"
+            aria-labelledby="main navigation"
+            className={classes.navDisplayFlex}
+          >
+            {kNavLinks.map(({ title, path }) => (
+              <Link to={path} key={title} className={classes.linkText}>
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default Header;
---
+++ portfolio/src/urls.ts
@@ -0,0 +1,7 @@
+const kNavLinks = [
+  { title: "Home", path: "/Portfolio/home" },
+  { title: "Projects", path: "/Portfolio/projects" },
+  { title: "Contact", path: "/Portfolio/contact" },
+];
+
+export default kNavLinks;
