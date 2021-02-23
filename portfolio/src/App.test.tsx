98f50388691ecbcb65f2b7696b198d134b6d57af
Tomas Lugo <lugo.lbmi@gmail.com>
2021-02-23 17:14:06 -0600

    Adds back App.test.tsx

---
+++ portfolio/src/App.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
+
+test("renders learn react link", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+});
