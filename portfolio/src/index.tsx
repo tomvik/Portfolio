31c39791753499c45b90dd4a4b09a3a175fe8da7
Tomas Lugo <lugo.lbmi@gmail.com>
2021-03-02 17:55:20 -0600

    Fixes redirecting

--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -17,7 +17,7 @@ const StyledWrapper = styled.div`
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/Portfolio'>
       <StyledWrapper>
         <Header />
         <App />
