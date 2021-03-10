f82709a1943023bfab2ea864b92a6d224b837c2a
Tomas Lugo <lugo.lbmi@gmail.com>
2021-03-10 16:31:58 -0600

    Moves footer icons to center using flex

--- portfolio/src/components/Footer/Footer.tsx
+++ portfolio/src/components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import MailIcon from "@material-ui/icons/Mail";
 import styled from "styled-components";
 import { kLinkedInLink, kGithubLink, kEmail } from "../../data/urls";
+import { makeStyles } from "@material-ui/core";
 
 // TODO: Verify later if it needs to be used or not.
 // Link of current answer: https://stackoverflow.com/questions/4575826/how-to-push-a-footer-to-the-bottom-of-page-when-content-is-short-or-missing
@@ -14,29 +15,39 @@ const FooterStyled = styled.div`
   z-index: 0;
 `;
 
+const useStyles = makeStyles({
+  footerDisplayFlex: {
+    display: `flex`,
+    justifyContent: `center`,
+  },
+});
+
 const Footer = () => {
   const color = "secondary";
+  const classes = useStyles();
 
   return (
     <FooterStyled>
       <AppBar position="static" color="primary">
         <Container maxWidth="md">
           <Toolbar>
-            <a href={kLinkedInLink} target="_blank" rel="noreferrer">
-              <IconButton edge="start" color={color} aria-label="home">
-                <LinkedInIcon fontSize="default" />
-              </IconButton>
-            </a>
-            <a href={kGithubLink} target="_blank" rel="noreferrer">
-              <IconButton edge="start" color={color} aria-label="home">
-                <GitHubIcon fontSize="default" />
-              </IconButton>
-            </a>
-            <a href={`mailto: ${kEmail}?subject = Contact&body = Message`}>
-              <IconButton edge="start" color={color} aria-label="home">
-                <MailIcon fontSize="default" />
-              </IconButton>
-            </a>
+            <Container className={classes.footerDisplayFlex}>
+              <a href={kLinkedInLink} target="_blank" rel="noreferrer">
+                <IconButton edge="start" color={color} aria-label="home">
+                  <LinkedInIcon fontSize="default" />
+                </IconButton>
+              </a>
+              <a href={kGithubLink} target="_blank" rel="noreferrer">
+                <IconButton edge="start" color={color} aria-label="home">
+                  <GitHubIcon fontSize="default" />
+                </IconButton>
+              </a>
+              <a href={`mailto: ${kEmail}?subject = Contact&body = Message`}>
+                <IconButton edge="start" color={color} aria-label="home">
+                  <MailIcon fontSize="default" />
+                </IconButton>
+              </a>
+            </Container>
           </Toolbar>
         </Container>
       </AppBar>
