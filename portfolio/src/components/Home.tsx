63a818c61cd6d1ba91bb55fe8f5b215554bf5eb3
Tomas Lugo <lugo.lbmi@gmail.com>
2021-05-23 19:17:14 -0500

    Adds home page

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -1,29 +1,23 @@
 import React from "react";
+import { Container, Paper } from "@material-ui/core";
+import { personal_info } from "./../data/Personal_information.json";
 
 const Home = () => {
   return (
-    <div>
-      <h2>Homeeee</h2>
-      <h2>Homeeeeee</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-      <h2>Home</h2>
-    </div>
+    <Container>
+      {personal_info.map((info) => {
+        return (
+          <Paper style={{ padding: 16, margin: 32 }} elevation={3}>
+            <h1>{info.title}</h1>
+            {info.text.map((text) => {
+              return (
+                <p>{text}</p>
+              )
+            })}
+          </Paper>
+        )
+      })}
+    </Container>
   );
 };
 
